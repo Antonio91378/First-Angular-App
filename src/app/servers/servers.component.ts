aa167290bdf97da5c4e43209f87998b1778f1ac9
Antonio <antoniovictor200@gmail.com>
2022-05-14 12:13:42 -0300

    aprendendo a mudar valores e atributos de componentes dinamicamente com angualar, constructor = useeffect em react

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = true
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = false
+    }, 2000)
   }
 
+  ngOnInit(): void {}
 }
